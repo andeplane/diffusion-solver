6fc2461744b09314129887e1421a569280a2d387
Anders Hafreager <andershaf@gmail.com>
2016-10-20 05:57:14 +0200

    Changed so Make.sh creates library

--- core/Make.sh
+++ core/Make.sh
@@ -1,1 +1,1 @@
-icpc -std=c++14 -I. -qopenmp -O3 -xHost *.cpp integrators/*.cpp modifiers/*.cpp -o diffusion
+icpc -xHost -single_module -dynamiclib -std=c++14 -I. -qopenmp -Ofast *.cpp integrators/*.cpp modifiers/*.cpp -o libcore.dylib
